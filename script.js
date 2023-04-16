00f1ba4ca76bce1a9db71feae259ede6b9d39cf4
Umesh Pal Singh <69305601+umeshpalsingh@users.noreply.github.com>
2023-04-17 02:24:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// let js = "Javascript is FUN"
+// if (js === 'js is fun ') {
+//     alert('omg')
+// } else if (js === "Javascript is FUN") {
+//     alert("I'm Smart.")
+// }
+// var result = 40 + 8 + 23 - 10;
+
+let firstName = 'Umesh';
+
+console.log(firstName);
+
+function openMenu() {
+    document.querySelector('.mobile_menu').classList.add('show');
+}
+
+function closeMenu() {
+    document.querySelector('.mobile_menu').classList.remove('show');
+}
+
